26f654582af9b752570d8734d763a4464dc86673
Stefan Schindler <tuxle@estada.ch>
2014-02-01 00:54:49 +0100

    Habe ein paar erste Zeilen für einen Demo-Client in Java erstellt.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 inchat-java
 ===========
+
+This is a demo Application to test inchat-common
--- src/inchat/java/InchatJava.java
+++ src/inchat/java/InchatJava.java
@@ -3,12 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package inchat.java;
+
+import java.util.UUID;
+import org.inchat.common.*;
 
 /**
  *
- * @author vp
  */
 public class InchatJava {
 
@@ -16,7 +17,18 @@ public class InchatJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InchatJava self = new InchatJava();
     }
-    
+
+    public InchatJava() {
+        
+        // This is me
+        Participant alice = new Participant(new UUID(
+                (int)(Math.random()*1024*1024)
+                , (int)(Math.random()*1024*1024)
+        ));
+        
+        Connection conn = InChatTransport.connect("remotehost");
+        
+    }
 }
